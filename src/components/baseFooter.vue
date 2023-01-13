fd23edc8322da0fdf96ccd2f70d997d61a7cefa9
Mocheng <18830279823@163.com>
2023-01-13 13:11:42 +0800

    页脚响应式解决

--- src/components/baseFooter.vue
+++ src/components/baseFooter.vue
@@ -1,27 +1,27 @@
 <template>
   <!--  页脚链接 -->
-  <div class="social_links">
+  <div :class="[!isHomePage ? 'social_links' : 'index_container']">
     <div class="sc-links_clone">
       <span>导航</span>
       <ul>
         <li>
           <a href="javascript:;">
-            <svg-icon name="bilibili" width="20" height="20"></svg-icon>
+            <svg-icon name="bilibili" :width="normalSize" :height="normalSize" color="#fff"></svg-icon>
           </a>
         </li>
         <li>
           <a href="javascript:;">
-            <svg-icon name="zhihu" width="20" height="20"></svg-icon>
+            <svg-icon name="zhihu" :width="normalSize" :height="normalSize" color="#fff"></svg-icon>
           </a>
         </li>
         <li>
           <a href="javascript:;">
-            <svg-icon name="github" width="20" height="20"></svg-icon>
+            <svg-icon name="github" :width="normalSize" :height="normalSize" color="#fff"></svg-icon>
           </a>
         </li>
         <li>
           <a href="javascript:;">
-            <svg-icon name="google" width="20" height="20"></svg-icon>
+            <svg-icon name="google" :width="normalSize" :height="normalSize" color="#fff"></svg-icon>
           </a>
         </li>
       </ul>
@@ -39,3 +39,59 @@
   </div>
   <!-- 指针结束 -->
 </template>
+
+<script setup>
+const route = useRoute()
+const isHide = ref(false)
+const isHomePage = ref(false)
+const normalSize = ref('20')
+onBeforeMount(() => {
+  resize()
+  window.onresize = resize
+})
+
+const resize = () => {
+  if (window.innerWidth >= 1066) {
+    isHide.value = false
+    normalSize.value = '18'
+  } else {
+    isHide.value = true
+    normalSize.value = '20'
+  }
+}
+
+watch(
+  () => route.meta.isHomePage,
+  (newVal) => {
+    if (newVal && !isHide.value) isHomePage.value = true
+    else {
+      isHomePage.value = false
+      normalSize.value = '20'
+    }
+  },
+)
+</script>
+
+<style lang="scss">
+.index_container {
+  position: fixed;
+  bottom: 33px;
+  left: 50px;
+  z-index: 20;
+  transition: all 0.2s ease-in-out;
+  font-size: 13px;
+  span {
+    margin-right: 10px;
+  }
+}
+
+.index_container .sc-links_clone {
+  display: flex;
+  ul {
+    display: flex;
+    li {
+      margin-left: 10px;
+    }
+  }
+}
+</style>
