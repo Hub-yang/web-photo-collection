5d6951f2f90943aca294c4b389a735eb7695cbe2
Mocheng <78254474+Mochenghualei@users.noreply.github.com>
2023-04-27 18:31:40 +0800

    测试cos

--- src/views/index/config.ts
+++ src/views/index/config.ts
@@ -8,115 +8,300 @@ type Img = {
 
 export const imgList: Img[] = [
   {
-    url: "https://s1.ax1x.com/2023/04/18/p9PaNTA.jpg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/001.jpeg",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/file1DpnZVhln5ENpinJhIJRA.JPG",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/002.jpeg",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/file2Rr06vhlcoFot32JVVl3F.JPG",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/003.jpeg",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/file35cwstaWinU8GPCjopuXZ.JPG",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/004.jpeg",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/file3YBV9wLFxsIqyGP6CXksT.jpeg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/005.jpg",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/file4cTbR_5hC6md5PP69GdS8.jpeg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/006.jpg",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/file7zII4Wma5Hb2pRt_vhqBE.JPG",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/007.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/file91f73E6QOgGsv_JiBT7R4.JPG",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/008.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/file9Cswv0eCW23K5eaPObSEB.JPG",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/009.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileEHhQWmty0-veHelll96Sg.jpg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/010.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileEPgOW9KPh_WVv-nKGNynF.jpg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/011.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileHkVGzMnXTInxpy6P6xGO-.jpeg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/012.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileJXCiZMU2jBF2y5VC59qID.jpeg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/013.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileKFix6JhJdCZdM1YcIkHrP.JPG",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/014.jpg",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileKNN_fLVPMRnV5sXFGyb3N.jpeg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/015.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileLVJy8bjApzEolqnn5sLk7.JPG",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/016.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  }, {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/017.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileOk7MuU2uFMk88gzUX1J6i.jpeg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/018.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileShsmIZ76aXdXa_sjnzWRk.jpeg",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/019.JPG",
     class: "景观",
     title: "shuai",
     catName: "景观"
   },
   {
-    url: "http://rt9kpxdep.bkt.clouddn.com/photos/fileTx0Ckre2G3-sx4k8hopcv.JPG",
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/020.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/021.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/022.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/023.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/024.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/025.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/026.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/027.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/028.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/029.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/030.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/031.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/032.jpg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/033.JPG",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/034.jpg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/035.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/036.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/037.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/038.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/039.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/040.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/041.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/042.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/043.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/044.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/045.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/046.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/047.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/048.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/049.jpeg",
+    class: "景观",
+    title: "shuai",
+    catName: "景观"
+  },
+  {
+    url: "https://personal-web-1308697453.cos.ap-beijing.myqcloud.com/%E6%91%84%E5%BD%B1%E4%BD%9C%E5%93%81%E5%B1%95%E5%9B%BE%E7%89%87/050.jpeg",
     class: "景观",
     title: "shuai",
     catName: "景观"
